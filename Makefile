5e2123b877d69ab77753ee718ec806ed85b596f0
Alexander Markov <45567992+moxicom@users.noreply.github.com>
2024-02-08 19:17:53 +0300

    wtf fix??git commit -m wtf fix??By following these steps, you should be able to remove and reinstall .NET using Snap on your Linux Mint system. If you encounter any issues or have further questions, feel free to ask!) xd) xd

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-.PHONY: run migrate
+.PHONY: run migration
 
 run:
 	go run cmd/main.go
 
 migrate:
-	go run cmd/main.go migrate
+	go run cmd/main.go migration
--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func main() {
 	if len(os.Args) >= 2 {
 		command := os.Args[1]
 		switch command {
-		case "migrate":
+		case "migration":
 			if err := repository.RunMigration(db); err != nil {
 				panic(err)
 			}
